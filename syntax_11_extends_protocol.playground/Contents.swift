4067902ac205ac67fff1cb535320fc5017caf067
Juwon-Yun <eooll2@daum.net>
2022-08-25 00:14:13 +0900

    modify: syntax_11_protocol_inherit.playground

--- syntax_11_extends_protocol.playground/Contents.swift
+++ syntax_11_extends_protocol.playground/Contents.swift
@@ -30,3 +30,54 @@ let friend = Friend(lastName: "jw", firstName: "y")
 print(friend.getName()) // jw
 
 print(friend.getFullname()) // jw y
+
+// protocol associated Type
+protocol PetHaving{
+    // associatedtype T란 T 타입을 갖고 있는 PetHaving으로 추상화 한다.
+    associatedtype T
+    var pets: [T] {get set}
+    mutating func gotNewPet(_ newPet : T)
+}
+
+extension PetHaving{
+    mutating func gotNewPet(_ newPet : T){
+        self.pets.append(newPet)
+    }
+}
+
+enum Animal {
+    case cat, dog, bird
+}
+
+struct petFriend : PetHaving {
+    mutating func gotNewPet(_ newPet: Animal) {
+        self.pets.append(newPet)
+    }
+    
+    var pets: [Animal] = []
+}
+
+struct petFamily : PetHaving {
+    mutating func gotNewPet(_ newPet: String) {
+        self.pets.append(newPet)
+    }
+    
+    var pets: [String] = []
+}
+
+var petFriend1 = petFriend()
+
+petFriend1.gotNewPet(Animal.bird)
+petFriend1.gotNewPet(Animal.cat)
+petFriend1.gotNewPet(Animal.dog)
+petFriend1.pets
+
+var petFamily1 = petFamily()
+
+petFamily1.gotNewPet("거북이")
+petFamily1.gotNewPet("토끼")
+petFamily1.gotNewPet("강아지")
+petFamily1.pets
+
+print(petFriend1.pets)
+print(petFamily1)
