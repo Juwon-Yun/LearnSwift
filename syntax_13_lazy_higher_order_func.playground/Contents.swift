d8360bb74de6a2d814e1d67d1f34b60596f0af8a
Juwon-Yun <eooll2@daum.net>
2022-08-27 20:22:17 +0900

    modify: syntax_13_lazy_higher_order_function.playground

--- syntax_13_lazy_higher_order_func.playground/Contents.swift
+++ syntax_13_lazy_higher_order_func.playground/Contents.swift
@@ -22,3 +22,28 @@ var someFriend = Friend("yjw")
 
 
 someFriend.pet
+
+// 고차함수
+// 매개변수로 함수 또는 클로저를 받고 받았던 매개변수를 변환하고 반환하는 함수
+// 콜렉션에 들어가는 배열, 딕셔너리, 셋
+// Sorted, Map, Filter, Reduce등이 빌트인 되어 있다.
+
+func getName(_ name : String) -> String{
+    return "my name is \(name)"
+}
+
+var getNameClosure : (String) -> String
+
+// 메소드인데 매개변수를 클로저로 받고 클로저에서 매개변수 값을 변경한다.
+func sayHello(getName : (String) -> String, name : String ) -> String{
+    return getName(name)
+}
+
+let resultHello = sayHello(getName: getName(_:), name : "yjw")
+
+// Swift에서의 map의 정의
+// 이런걸 고차함수라고 한다.
+//@inlinable public func map<T>(_ transform: (Element) throws -> T) rethrows -> [T]
+
+
+
