2c2b0f644c27762901dccc3d9235b6db6c216408
Juwon-Yun <eooll2@daum.net>
2022-08-27 23:34:54 +0900

    modify: SortVIew.swift

--- LearnSwift/SortView/SortView.swift
+++ LearnSwift/SortView/SortView.swift
@@ -6,15 +6,16 @@
 //
 
 import SwiftUI
+import Foundation
 
 struct Todo : Comparable, Hashable {
     static func < (lhs: Todo, rhs :Todo) -> Bool{
-        return lhs.id < rhs.id
-        //        return lhs.createdAt < rhs.createdAt
+//        return lhs.id < rhs.id
+                return lhs.createdAt < rhs.createdAt
     }
     
-    var id : Int
-    //    var createdAt : Date
+//    var id : Int
+    var createdAt : Date
     var title : String
 }
 
@@ -22,11 +23,18 @@ struct SortView: View {
     @State var todos : [Todo] = []
     @State var isAscending : Bool = true
     
+    var getToggleTitle : (Bool) -> String = {isAscending in
+        return isAscending ? "오름차순" : "내림차순"
+    }
+    
     var body: some View {
         VStack{
-            Toggle("toggle asc, desc", isOn: $isAscending)
+            Toggle(getToggleTitle(isAscending) , isOn: $isAscending)
                 .padding(.horizontal, 20)
             
+//            Toggle(isAscending ? "Desc" : "Asc" , isOn: $isAscending)
+//                .padding(.horizontal, 20)
+
             listContent
             
         }
@@ -46,8 +54,6 @@ struct SortView: View {
 }
 
 extension SortView{
-    
-    
     var listContent : some View{
         List{
             
@@ -65,7 +71,7 @@ extension SortView {
         var todos : [Todo] = []
         
         for i in 0...100{
-            let newItem = Todo(id: i, title: "\(i) 번째 타이틀 ")
+            let newItem = Todo(createdAt: Date.now, title: "\(i) 번째 타이틀 \(Date.now)")
             todos.append(newItem)
         }
         return todos
