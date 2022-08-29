de412ed3badf300a68618d60b5509c33cd8af7da
Juwon-Yun <eooll2@daum.net>
2022-08-29 22:14:06 +0900

    modify: syntax_15_private_setter_foreach_index.playground

--- syntax_15_private_setter_foreach_index.playground/Contents.swift
+++ syntax_15_private_setter_foreach_index.playground/Contents.swift
@@ -28,3 +28,17 @@ myPet.setName(to: "바뀐 이름")
 myPet.name
 
 // Alamofire에서도 주로 prevate (set)을 사용한다.
+
+
+// foreach index
+
+let array : [String] = ["철", "수", "영", "희", "마"]
+
+var freindWithIndex : [String] = []
+
+for (index, element) in array.enumerated() {
+    print("index: \(index), item : \(element)")
+    freindWithIndex.append("\(index)번째 \(element)")
+}
+
+print(freindWithIndex)
