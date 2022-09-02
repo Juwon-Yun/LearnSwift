856bec50f3b94caa478f4824be3615aab2cb5a44
Juwon-Yun <eooll2@daum.net>
2022-09-03 00:47:25 +0900

    modify: syntax_18_dictionary_grouping.playground

--- syntax_18_dictionary_grouping.playground/Contents.swift
+++ syntax_18_dictionary_grouping.playground/Contents.swift
@@ -3,3 +3,31 @@ import UIKit
 enum FriendType {
     case normal, best
 }
+
+struct Friend {
+    var name : String
+    var type : FriendType
+}
+
+
+var friendList = [
+    Friend(name : "철수01", type: .normal),
+    Friend(name : "철수02", type: .best),
+    Friend(name : "철수03", type: .normal),
+    Friend(name : "철수04", type: .best),
+]
+
+// 들어온 키가 같으면 따로 분류한다.
+//let groupFriends = Dictionary(grouping: friendList, by: {$0.type})
+
+// 여러가지 형태로 그룹핑이 가능하다
+// Date나 Data내용으로 그룹핑한다.
+let groupFriends = Dictionary(grouping: friendList, by: {
+    (friend) -> FriendType in
+    return friend.type
+})
+
+
+
+print(groupFriends[.normal])
+print(groupFriends[.best])
