5cb859b745ea035c6efb888552aed0e68144bd2c
Juwon-Yun <eooll2@daum.net>
2022-09-09 20:44:42 +0900

    feature: AnyPublisher

---
+++ LearnSwift/combine_02_uikit.swift
@@ -0,0 +1,40 @@
+//
+//  combine_02_uikit.swift
+//  LearnSwift
+//
+//  Created by 윤주원 on 2022/09/09.
+//
+
+import UIKit
+import Combine
+
+class ViewController : UIViewController{
+    // ctrl + drag로 스토리보드에서 가져올 수 있다.
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var passwordConfirmTextField: UITextField!
+    @IBOutlet weak var submitBtn: UIButton!
+    
+    var viewModel : CombineViewModel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad();
+        
+        viewModel = CombineViewModel()
+    }
+}
+
+extension UITextField{
+    
+    var textPublisher : AnyPublisher<String, Never>{
+        
+        // eraseToAnyPublisher() : AnyPublisher로 설정한다.
+        NotificationCenter.default.publisher(for: UITextField.textDidChangeNotification, object: self)
+            // UITextField를 가져온다.
+            .compactMap{ $0.object as? UITextField}
+            // String을 가져온다
+            .map{$0.text ?? ""}
+            .print("text ::: ")
+            // Publisher 타입을 AnyPublisher로 설정한다.
+            .eraseToAnyPublisher()
+    }
+}
